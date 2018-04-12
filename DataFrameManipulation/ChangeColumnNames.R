7575e404f662b43b8dd23ff70d573969c8ece5d9
Cam <camillathorn85@gmail.com>
2018-04-12 16:27:26 +0100

    Create ChangeColumnNames.R

---
+++ DataFrameManipulation/ChangeColumnNames.R
@@ -0,0 +1,11 @@
+# Change column names
+
+# if you want to change the column name based on the old column name
+# instead of saying a number which column it is.
+colnames(dataframe)[which(names(dataframe) == "columnName")] <- "newColumnName"
+
+# by column number 
+colnames(dataframe)[2] <- "NewColName
+
+
+
