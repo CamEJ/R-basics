87a8d039368ab7647d693d23219183d98b9489c7
Cam <camillathorn85@gmail.com>
2020-07-27 14:22:07 +0100

    Update changing facet wrap labels.R

--- ggplotAesthetics/changing facet wrap labels.R
+++ ggplotAesthetics/changing facet wrap labels.R
@@ -24,4 +24,33 @@ plot + theme(
           )
 
 
+## subscript & other scientific notation in facet labels
+# basically by using labeller = label_parsed in facet wrap after id'ing labels
+
+# here a df called AvGHG which looks like this
+#     Day trt     Gas     MEAN      SE       unit                                
+#     0 control F_CH4  -0.00733   0.000212  g/m2/d        
+#     0 control F_CO2  196.       11.0      g/m2/d  
+
+# with the data, i want to plot a line chart of  mean gas over time (Day)
+# with data facet-wrapped by the different gases measured
+# normally, would just do facet_wrap(~Gas) but i want after gas name
+# the unit meaured with superscript in instead of per kg for example. 
+# so i add a variable called 'facets' to my AvGHG df where i put IN THE CORRECT ORDER
+# the new labels I want along with units and scientific notation
+AvGHG$facets = factor(AvGHG$Gas, labels = c(
+  "Methane~(g~m^{-2}~day^{-1})", 
+  "Carbon~dioxide~(g~m^{-2}~day^{-1})", 
+  "Nitrous~oxide~(mg~m^{-2}~day^{-1})"))
+
+# now plot as normal and add 'label_parsed'argument
+ ggplot(AvGHG, aes(x=Day, y=MEAN, colour=trt)) +
+  geom_line(size=0.8) +
+  geom_errorbar(aes(ymin=MEAN-SE, ymax=MEAN+SE), width=.25) +
+  geom_point(size=2,color='black', shape=21, aes(fill=trt)) +
+  facet_wrap(~facets, labeller = label_parsed,scales="free_y",ncol=1)
+
+## note: make sure labels in correct order or you will wrongly label your facets
+# should always plot raw version first w/o label edits as a reference.
+
 
